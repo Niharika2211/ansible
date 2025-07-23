fa4c048b506bf947c225c508f2f1bd466b789b19
Niharika2211 <niharika2211@gmail.com>
2025-07-23 16:44:15 +0530

    ans.sh

---
+++ prod-ansi/ansbile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+# update 
+sudo yum update -y
+
+
+#installing ansible in amazon linux
+sudo yum install amazon-linux-extras install ansible -y
+
+
+#ansible version
+ansible version
