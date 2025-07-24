9a2539d708b4fe45d567f649a9e1881e23cac9df
Niharika2211 <niharika2211@gmail.com>
2025-07-24 16:20:32 +0530

    pems

--- prod-ansi/ansbile.sh
+++ prod-ansi/ansbile.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+    #!/bin/bash
 
 
 
-# update 
-sudo yum update -y
+    # update 
+    sudo yum update -y
 
+ 
+    #install amazon-extras 
+    sudo yum install amazon-linux-extras -y
+    
+    
+    #installing ansible in amazon linux
+    sudo yum install ansible -y 
 
-#installing ansible in amazon linux
-sudo yum install amazon-linux-extras install ansible -y
-
-
-#ansible version
-ansible version
+    #ansible version
+    ansible version
